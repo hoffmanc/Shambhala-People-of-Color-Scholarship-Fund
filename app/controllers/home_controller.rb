7110b60e8bfedb1d11634751f601da091a34fd19
chris hoffman <hoffmanc@dostoevsky.hsd1.nh.comcast.net>
2010-08-23 06:59:52 -0400

    got pages working, and printing main menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   layout 'site'
+
   def index
-    @main_pages = Page.find(:all, :params => { :parent => nil })
+    @main_pages = Page.find(:all, :conditions => { :parent_id => nil })
+    @conf = { :site_name => "Shambhala People of Color Scholarship Fund" }
     respond_to do |format| 
       format.html # index.html.erb  
       format.xml { render :xml => @posts } 
