90b84feaf51e5e8ce01cce2028538da124931f1c
chris hoffman <hoffmanc@dostoevsky.hsd1.nh.comcast.net>
2010-09-19 08:51:05 -0400

    flash now displaying everywhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_helper
+    r = [:notice, :warning, :message, :error].map do |f| 
+      content_tag(:div, flash[f], :class => f) if flash[f] 
+    end
+    r
+  end
 end
